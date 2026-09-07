1e616150b6a9e0ed991720ab649f6df33c3ce15f
markgithinji <57723258+markgithinji@users.noreply.github.com>
2026-09-07 18:21:30 +0300

    build: implement feature and application convention plugins for modular standardization

---
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,22 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.snap.convention.configureAndroidCompose
+import com.snap.convention.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+            }
+
+            extensions.configure<ApplicationExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = ProjectConfig.targetSdk
+                configureAndroidCompose(this)
+            }
+        }
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,26 @@
+import com.snap.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("snap.android.library")
+                apply("snap.android.compose")
+                apply("snap.android.hilt")
+            }
+
+            dependencies {
+                "implementation"(project(":core:domain"))
+                "implementation"(project(":core:common"))
+                "implementation"(project(":core:ui"))
+
+                "implementation"(libs.findLibrary("androidx-lifecycle-runtime-ktx").get())
+                "implementation"(libs.findLibrary("androidx-activity-compose").get())
+                "implementation"(libs.findLibrary("androidx-hilt-navigation-compose").get())
+            }
+        }
+    }
+}
