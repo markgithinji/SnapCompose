f06b70f9651abc8be5c1987db7dcb2c430eedd16
markgithinji <57723258+markgithinji@users.noreply.github.com>
2026-09-07 03:37:45 +0300

    feat(home): move data hilt module into the data package for better grouping

--- feature/home/src/main/java/com/example/composegallery/feature/home/data/DataModule.kt
+++ feature/home/src/main/java/com/example/composegallery/feature/home/data/DataModule.kt
@@ -1,4 +1,4 @@
-package com.example.composegallery.feature.home.di
+package com.example.composegallery.feature.home.data
 
 import com.example.composegallery.core.domain.repository.FavoriteRepository
 import com.example.composegallery.core.domain.repository.GalleryRepository
