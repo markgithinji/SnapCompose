a2a03279a83c374fbd8c5d82ffca8d24c5284960
markgithinji <57723258+markgithinji@users.noreply.github.com>
2026-09-07 18:20:06 +0300

    build: implement snap.android.compose convention plugin for jetpack compose setup

---
+++ build-logic/convention/src/main/kotlin/com/snap/convention/AndroidCompose.kt
@@ -0,0 +1,21 @@
+package com.snap.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureAndroidCompose(
+    commonExtension: CommonExtension,
+) {
+    commonExtension.apply {
+        buildFeatures.compose = true
+    }
+
+    pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
+
+    dependencies {
+        val bom = libs.findLibrary("androidx-compose-bom").get()
+        add("implementation", platform(bom))
+        add("androidTestImplementation", platform(bom))
+    }
+}
